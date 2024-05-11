16d3660457442aea70e2ba9591dcbdb776b67d83
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-05-11 20:43:50 +0900

    Add files via upload

---
+++ 0522-node2-app/basic-q1-route-app/server-1.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    res.setHeader("Content-Type", "text/plain");
+    res.write("Hello Node");
+    res.end();
+   // console.log(req);
+});
+
+server.listen(3000, () => {
+    console.log("3000번 포트에서 서버 실행 중");
+});
