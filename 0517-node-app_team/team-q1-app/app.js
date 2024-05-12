80ebe250153b1f81c79dbf0ebb9c35355af920ba
KBMultiKDT-강사용 <168627343+KBMultiKDT@users.noreply.github.com>
2024-05-12 21:45:21 +0900

    Delete 0517-node-app_team directory

--- 0517-node-app_team/team-q1-app/app.js
+++
@@ -1,6 +0,0 @@
-const person = require("./user2.js");
-
-console.log(person.name);
-console.log(person.age);
-console.log(person.tel);
-console.log(person);
--- 0517-node-app_team/team-q1-app/user.js
+++
@@ -1,4 +0,0 @@
-const user1 = "Kim";
-const user2 = "Lee";
-const user3 = "Choi";
-module.exports = {user1, user2, user3};
